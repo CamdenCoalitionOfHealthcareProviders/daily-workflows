7394b0f7b0489c660fbd0b12117cf326accca240
Jay Fisher <fisher.jc@gmail.com>
2017-01-25 09:28:35 -0500

    edit a pra + formatting changes

--- r-scripts/pra.R
+++ r-scripts/pra.R
@@ -2,13 +2,13 @@
 library(reshape)
 
 # Reads in files
-pra     <-read.csv(paste("tmp/pra",".csv", sep=""), stringsAsFactors=FALSE)
+pra <-read.csv(paste("tmp/pra",".csv", sep=""), stringsAsFactors=FALSE)
 caplist <-read.csv(paste("tmp/caplist",  ".csv", sep=""), stringsAsFactors=FALSE)
 
 # Subsets only those with a Subscriber ID and with a PRA Creation Date, and those who don't have NIC in their Subscriber ID
-pra2<-subset(pra,Subscriber.ID!="" )
-pra3<-subset(pra2,PRA.Creation.Date!="" )
-pra4<-subset(pra3,!grepl("NIC", pra3$Subscriber.ID))
+pra2 <- subset(pra,Subscriber.ID!="" )
+pra3 <- subset(pra2,PRA.Creation.Date!="" )
+pra4 <- subset(pra3,!grepl("NIC", pra3$Subscriber.ID))
 
 # Remove "U" from string to match TrackVia Subscriber IDs
 pra4$Subscriber.ID<-gsub("U", "", pra4$Subscriber.ID)
@@ -18,14 +18,15 @@ pra4$PRA.Facility.Created[pra4$PRA.Facility.Created == "Camcare Gateway"] <- "CA
 pra4$PRA.Facility.Updated[pra4$PRA.Facility.Updated == "Camcare Gateway"] <- "CAMcare Gateway"
 pra4$PRA.Facility.Created[pra4$PRA.Facility.Created == "CAMCare Gateway"] <- "CAMcare Gateway"
 pra4$PRA.Facility.Updated[pra4$PRA.Facility.Updated == "CAMCare Gateway"] <- "CAMcare Gateway"
+pra4$PRA.Facility.Created[pra4$PRA.Facility.Created == "CAMcare - Clementon"] <- "CAMcare Clementon"
+pra4$PRA.Facility.Updated[pra4$PRA.Facility.Updated == "CAMcare - Clementon"] <- "CAMcare Clementon"
+
 
 # Removes commas from values in the facility field
 pra4$PRA.Facility.Created[pra4$PRA.Facility.Created == "Shlomo Stemmer, M.D."] <- "Shlomo Stemmer"
 pra4$PRA.Facility.Updated[pra4$PRA.Facility.Updated == "Shlomo Stemmer, M.D."] <- "Shlomo Stemmer"
 pra4$PRA.Facility.Created[pra4$PRA.Facility.Created == "Prasanta C. Chandra, M.D."] <- "Prasanta C Chandra MD"
 pra4$PRA.Facility.Updated[pra4$PRA.Facility.Updated == "Prasanta C. Chandra, M.D."] <- "Prasanta C Chandra MD"
-
-# Removes commas from values in the facility field
 pra4$PRA.Facility.Created[pra4$PRA.Facility.Created == "Southern Jersey Family Medical Centers, Inc. - Burlington"] <- "Southern Jersey Family Medical Centers Burlington"
 pra4$PRA.Facility.Updated[pra4$PRA.Facility.Updated == "Southern Jersey Family Medical Centers, Inc. - Burlington"] <- "Southern Jersey Family Medical Centers Burlington"
 pra4$PRA.Facility.Updated[pra4$PRA.Facility.Updated == "Martin E. Kanoff, DO, FACOOG"] <- "Martin E Kanoff DO"
@@ -52,13 +53,13 @@ pra4$PRA.Facility.Created[pra4$PRA.Facility.Created == "Women's Health Associate
 pra4$PRA.Facility.Updated[pra4$PRA.Facility.Updated == "Women's Health Associates - Turnersville"] <- "Womens Health Associates - Turnersville"
 
 # Keeps only those that have a corresponding Subscriber ID
-pra4<-subset(pra4, (pra4$Subscriber.ID %in% caplist$SUBSCRIBER_ID))
+pra4 <- subset(pra4, (pra4$Subscriber.ID %in% caplist$SUBSCRIBER_ID))
 
 # Convert PRA PATID values to lower
-pra4$PRA.PATID<-tolower(pra4$PRA.PATID)
+pra4$PRA.PATID <- tolower(pra4$PRA.PATID)
 
 # Identifies fields for export
-pra5<-pra4[,c("HIE.ID", 
+pra5 <- pra4[,c("HIE.ID", 
               "PRA.Creation.Date", 
               "PRA.Facility.Created", 
               "Most.Recent.Update.Date", 
@@ -66,12 +67,12 @@ pra5<-pra4[,c("HIE.ID",
               "PRA.PATID")]
 
 # Renames fields
-pra5<-reshape::rename(pra5, c(HIE.ID="HIEID"))
-pra5<-reshape::rename(pra5, c(PRA.Creation.Date="PRA Creation Date"))
-pra5<-reshape::rename(pra5, c(PRA.Facility.Created="PRA Facility Created"))
-pra5<-reshape::rename(pra5, c(Most.Recent.Update.Date="Most Recent Update Date"))
-pra5<-reshape::rename(pra5, c(PRA.Facility.Updated="PRA Facility Updated"))
-pra5<-reshape::rename(pra5, c(PRA.PATID="PRA PATID"))
+pra5 <- reshape::rename(pra5, c(HIE.ID="HIEID"))
+pra5 <- reshape::rename(pra5, c(PRA.Creation.Date="PRA Creation Date"))
+pra5 <- reshape::rename(pra5, c(PRA.Facility.Created="PRA Facility Created"))
+pra5 <- reshape::rename(pra5, c(Most.Recent.Update.Date="Most Recent Update Date"))
+pra5 <- reshape::rename(pra5, c(PRA.Facility.Updated="PRA Facility Updated"))
+pra5 <- reshape::rename(pra5, c(PRA.PATID="PRA PATID"))
 
 # Exports csv files
 # write.csv(pra5, (file=paste ("PRA-Table", ".csv", sep="")), row.names=FALSE)
